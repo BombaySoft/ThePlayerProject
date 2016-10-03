a43401133951d84b487852af4a7477fa81c6a1a3
BombaySoft <justvovka@gmail.com>
2016-10-03 22:00:06 +0300

    Tabs added

--- app/src/main/java/ru/bombaysoft/theplayer/MainActivity.java
+++ app/src/main/java/ru/bombaysoft/theplayer/MainActivity.java
@@ -2,6 +2,10 @@ package ru.bombaysoft.theplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
+
+import static android.R.attr.id;
+import static android.R.id.tabhost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("tab1");
+        tabSpec.setContent(R.id.tab1);
+        tabSpec.setIndicator("My Music");
+        tabHost.addTab(tabSpec);
+
+        TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("tab2");
+        tabSpec2.setContent(R.id.tab2);
+        tabSpec2.setIndicator("Search");
+        tabHost.addTab(tabSpec2);
+
+        TabHost.TabSpec tabSpec3 = tabHost.newTabSpec("tab3");
+        tabSpec3.setContent(R.id.tab3);
+        tabSpec3.setIndicator("History");
+        tabHost.addTab(tabSpec3);
     }
 }
